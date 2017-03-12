b29cf868e32179c9f001dc7a2706920bc4873541
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-13 10:33:22 +1100

    Add toolbar and refactor auth

---
+++ app/controllers/signin.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  email: null,
+  password: null,
+  showErrorDialog: false,
+
+  signInDisabled: Ember.computed('email', 'password', function() {
+    return (this.get('email') === null || this.get('password') === null);
+  }),
+
+  actions: {
+    signIn: function() {
+      const email = this.get('email');
+      const password = this.get('password');
+      const context = this;
+
+      this.get('session').open('firebase', {
+        provider: 'password',
+        email: email,
+        password: password
+      }).then(function(data) {
+        console.log(data.currentUser);
+        context.transitionToRoute('board');
+      }).catch(function(error) {
+        console.log(error);
+        context.set('showErrorDialog', true);
+      });
+    },
+
+    signOut: function() {
+      this.get('session').close();
+    },
+
+    closeErrorDialog: function() {
+      this.set('showErrorDialog', false);
+    }
+  }
+});
---
+++ app/routes/application.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel: function() {
+    return this.get('session').fetch().catch(function() {});
+  }
+});
