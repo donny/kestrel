5bb1e52e0552ee282a083298b88c446eebd470aa
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-14 08:42:51 +1100

    Add card plus one function

--- app/components/kestrel-card/component.js
+++ app/components/kestrel-card/component.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  actions: {
+    increaseScore: function() {
+      const store = this.get('store');
+      const cardId = this.get('card.id');
+
+      store.findRecord('card', cardId).then(function(card) {
+        card.incrementProperty('score');
+        card.save();
+      });
+    }
+  }
 });
